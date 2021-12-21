5c60a5348019244efee5b2f922989afefe4de4b8
Tim Honchel <tim.honchel@gmail.com>
2021-12-21 15:22:17 -0500

    first commit

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace PracticeLinq
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var gameList = new List<string>() { "Call of Duty", "Madden", "Grand Theft Auto", "Mario Kart", "Star Fox", "FIFA", "Halo", "Mortal Kombat" };
+            gameList.OrderBy(x => x.Length).ToList().ForEach(x => Console.WriteLine(x));
+        }
+    }
+}
